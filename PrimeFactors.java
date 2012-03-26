77f582e4d3bd669901d1294512c6fd46b8b5dd19
Ryan Fuller <fullerra@rose-hulman.edu>
2012-03-26 14:02:15 -0400

    Changed class names and solved testEight

---
+++ PrimeFactors.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+
+public class PrimeFactors {
+
+	public static ArrayList<Integer> generateFactors(int i) {
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		
+		while (i % 2 == 0) {
+			ret.add(2);
+			i /= 2;
+		}
+		
+		if (i > 1) {
+			ret.add(i);
+		}
+		return ret;
+	}
+
+}
---
+++ PrimeFactorsTest.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class PrimeFactorsTest {
+	
+	@Test
+	public void testJUnit() {
+		assertTrue("blah", true);
+	}
+
+	@Test
+	public void testOne() {
+		assertEquals(list(), PrimeFactors.generateFactors(1));
+	}
+	
+	@Test
+	public void testTwo() {
+		assertEquals(list(2), PrimeFactors.generateFactors(2));
+	}
+	
+	@Test
+	public void testThree() {
+		assertEquals(list(3), PrimeFactors.generateFactors(3));
+	}
+	
+	@Test
+	public void testFour() {
+		assertEquals(list(2, 2), PrimeFactors.generateFactors(4));
+	}
+	
+	@Test
+	public void testSix() {
+		assertEquals(list(2, 3), PrimeFactors.generateFactors(6));
+	}
+	
+	@Test
+	public void testEight() {
+		assertEquals(list(2, 2, 2), PrimeFactors.generateFactors(8));
+	}
+	
+	private ArrayList<Integer> list(int... inta) {
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for (int i : inta) {
+			ret.add(i);
+		}
+		return ret;
+	}
+}
