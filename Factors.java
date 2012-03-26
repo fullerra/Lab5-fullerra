ad8efb486dafe10b0f8f54cfed60f69cc67dc187
Ryan Fuller <fullerra@rose-hulman.edu>
2012-03-26 13:22:10 -0400

    Solved testOne

--- Factors.java
+++ Factors.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 public class Factors {
 
 	public static ArrayList<Integer> generateFactors(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<Integer>();
 	}
 
 }
