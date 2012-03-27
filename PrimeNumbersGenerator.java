bef06ecf91de55b06d8e305601f5a25f10013822
Ryan Fuller <fullerra@rose-hulman.edu>
2012-03-27 14:13:37 -0400

    passed testOne

--- PrimeNumbersGenerator.java
+++ PrimeNumbersGenerator.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 public class PrimeNumbersGenerator {
 
 	public static ArrayList<Integer> generatePrimeNumbers(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<Integer>();
 	}
 
 }
