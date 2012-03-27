92f29bbeec73f5dfab58e97031a678442dc802e7
Ryan Fuller <fullerra@rose-hulman.edu>
2012-03-27 14:12:37 -0400

    wrote testOne

--- PrimeNumbersGenerator.java
+++ PrimeNumbersGenerator.java
@@ -1,5 +1,12 @@
 package PrimeNumbersGeneration;
 
+import java.util.ArrayList;
+
 public class PrimeNumbersGenerator {
+
+	public static ArrayList<Integer> generatePrimeNumbers(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- PrimeNumbersGeneratorTest.java
+++ PrimeNumbersGeneratorTest.java
@@ -2,6 +2,8 @@ package PrimeNumbersGeneration;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class PrimeNumbersGeneratorTest {
@@ -9,6 +11,19 @@ public class PrimeNumbersGeneratorTest {
 	@Test
 	public void testJUnit() {
 		assertTrue("blah", true);
+	}
+	
+	@Test
+	public void testOne() {
+		assertEquals(list(), PrimeNumbersGenerator.generatePrimeNumbers(1));
+	}
+	
+	private ArrayList<Integer> list(int... inta) {
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for (int i : inta) {
+			ret.add(i);
+		}
+		return ret;
 	}
 
 }
